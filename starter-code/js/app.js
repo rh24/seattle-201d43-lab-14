77d5980ec5fa8f025a39f0bc5a8bb594c026d543
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-16 14:06:45 -0700

    Save item to local storage after adding to cart

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -9,11 +9,11 @@ var Cart = function(items) {
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
   this.items.push(new CartItem(product, quantity));
-  debugger;
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart', JSON.stringify(this.items));
 };
 
 Cart.prototype.removeItem = function(item) {
